77ab8307a2b6dcc48326e870ce7876fbf70b9e49
dicompathakofficial <65360046+dicompathakofficial@users.noreply.github.com>
2020-05-17 22:08:51 +0530

    Removing unnecessary spaces

--- script.py
+++ script.py
@@ -1,9 +1,15 @@
 ###### PROBLEM SET ######
-# Task 1: Write a function that can censor not just a specific word or phrase from a body of text, but a whole list of words and phrases, and then return the text.
-# Task 2: Write a function that can censor any occurance of a word from the “negative words” list after any “negative” word has occurred twice, as well as censoring everything from the list from the previous step as well.
+# Task 1: Write a function that can censor not just a specific word or phrase from a body of text, 
+# but a whole list of words and phrases, and then return the text.
+
+# Task 2: Write a function that can censor any occurance of a word from the 
+#“negative words” list after any “negative” word has occurred twice, as well 
+#as censoring everything from the list from the previous step as well.
 
 
-# These are the emails you will be censoring. The open() function is opening the text file that the emails are contained in and the .read() method is allowing us to save their contexts to the following variables:
+# These are the emails you will be censoring. The open() function is opening 
+#the text file that the emails are contained in and the .read() method is allowing us
+#to save their contexts to the following variables:
 
 email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
@@ -18,10 +24,15 @@ lower_case_email_four = email_four.lower()
 
 # Words to work on/variables
 word_to_censor = ["learning algorithms"]
-proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her",
+
+proprietary_terms = ["she", "personality matrix", "sense of self", 
+                     "self-preservation", "learning algorithm", "her",
                      "herself"]
-negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help",
-                  "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed",
+
+negative_words = ["concerned", "behind", "danger", "dangerous", 
+                  "alarming", "alarmed", "out of control", "help",
+                  "unhappy", "bad", "upset", "awful", "broken", "damage", 
+                  "damaging", "dismal", "distressed",
                   "distressed", "concerning", "horrible", "horribly", "questionable"]
 
 
@@ -53,7 +64,8 @@ def censorship(lower_case_email):
     return email
 
 
-# CODE on removing both negative words occuring twie or more than twice and censoring everything on proprietory_terms variable
+# CODE on removing both negative words occuring twie or more than twice and 
+#censoring everything on proprietory_terms variable
 
 def negativity(lower_case_email):
     email = lower_case_email
